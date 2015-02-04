1d2a602a6c2b4865507db300e75cb08f4413ee52
Matthieu Wipliez <matthieu.wipliez@synflow.com>
2015-02-04 12:07:29 +0100

    (sobel) using a renderer to show something on target image

--- Sobel/demo/src/sobel_filter.c
+++ Sobel/demo/src/sobel_filter.c
@@ -32,8 +32,12 @@ static SDL_Window *showSourceImage() {
 	return window;
 }
 
-static SDL_Window *createTargetWindow() {
-	SDL_Window *window = SDL_CreateWindow("target image", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 512, 512, 0);
+static SDL_Window *createTargetWindow(SDL_Window *winSource) {
+	int x, y, w, h;
+	SDL_GetWindowPosition(winSource, &x, &y);
+	SDL_GetWindowSize(winSource, &w, &h);
+
+	SDL_Window *window = SDL_CreateWindow("target image", x + w + 20, y, 512, 512, 0);
 	if (window == NULL) {
 		// In the event that the window could not be made...
 		printf("Could not create window: %s\n", SDL_GetError());
@@ -53,21 +57,32 @@ int main(int argc, char** a2rgv) {
 	atexit(SDL_Quit);
 
 	SDL_Window *winSource = showSourceImage();
-	SDL_Window *winTarget = createTargetWindow();
+	SDL_Window *winTarget = createTargetWindow(winSource);
+	SDL_Renderer *renderer = SDL_CreateRenderer(winTarget, -1, SDL_RENDERER_ACCELERATED);
+	SDL_RenderClear(renderer);
 
+	int i = 0;
 	while (1) {
 		SDL_Event event;
 		SDL_WaitEvent(&event);
-		if (event.type == SDL_QUIT) {
+		if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE) {
+			// closing any of the windows quits the application
 			break;
 		}
-		else if (event.type == SDL_WINDOWEVENT) {
-			if (event.window.event == SDL_WINDOWEVENT_CLOSE) {
-				// closing any of the windows quits the application
-				break;
-			}
-		}
+
+		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+		SDL_Rect rectangle;
+
+		rectangle.x = i;
+		rectangle.y = i;
+		rectangle.w = 50;
+		rectangle.h = 50;
+		SDL_RenderFillRect(renderer, &rectangle);
+		SDL_RenderPresent(renderer);
+		i = (i + 1) % 400;
 	}
+
+	SDL_DestroyRenderer(renderer);
 
 	// Close and destroy windows
 	SDL_DestroyWindow(winSource);
