bcae1935fd45d35c74bae004346cb4b7e4107b37
Matthieu Wipliez <matthieu.wipliez@gmail.com>
2017-09-01 23:24:21 +0200

    working Rust version

--- Sobel/demo/src/main.rs
+++ Sobel/demo/src/main.rs
@@ -1,16 +1,17 @@
 extern crate image;
+extern crate json;
 
 use std::fs::File;
 use std::io::{BufRead, BufReader, Read, Write};
 use std::path::Path;
 use std::process::{Command, Stdio};
-use std::thread;
+use std::{thread, time};
 
-use image::{GenericImage, Pixel};
+use image::{GenericImage, Pixel, ImageBuffer};
 
 fn main() {
     let mut child = Command::new("java")
-            .args(&["-cp", "../sim/classes", "sobel.SobelFilterTest"])
+            .args(&["-cp", "../sim/classes", "sobel.SobelFilter_test"])
             .stdin(Stdio::piped())
             .stdout(Stdio::piped())
             .spawn()
@@ -18,16 +19,24 @@ fn main() {
 
     let stdin = child.stdin.as_mut().unwrap();
     let mut stdout = BufReader::new(child.stdout.take().unwrap());
+    let img = image::open(&Path::new("data/lena512.bmp")).unwrap();
+    let (width, height) = img.dimensions();
 
     let child = thread::spawn(move || {
-        // some work here
         let mut buf = String::new();
-        println!("reading from output");
-        stdout.read_line(&mut buf).unwrap();
-        println!("read line: {}", buf);
+        let mut img = ImageBuffer::new(width, height);
+        for y in 0..height {
+            for x in 0..width {
+                stdout.read_line(&mut buf).unwrap();
+                let value = json::parse(&buf).unwrap().pop().as_u8().unwrap();
+                buf.clear();
+                img.put_pixel(x, y, image::Luma([value]));
+            }
+            println!("y = {}", y);
+        }
+        img.save(&Path::new("data/out.png")).unwrap();
     });
 
-    let img = image::open(&Path::new("data/lena512.bmp")).unwrap();
     for (x, y, pixel) in img.pixels() {
         stdin.write(format!("[{}]\n", pixel.to_luma()[0]).as_bytes()).unwrap();
         stdin.flush().unwrap();
