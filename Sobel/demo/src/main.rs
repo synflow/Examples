823b4cc8c02f6c5787f767a2ff55e47d3c4b98d2
Matthieu Wipliez <matthieu.wipliez@gmail.com>
2017-09-01 23:31:55 +0200

    end simulation after image is written

--- Sobel/demo/src/main.rs
+++ Sobel/demo/src/main.rs
@@ -17,33 +17,37 @@ fn main() {
             .spawn()
             .expect("failed to execute process");
 
-    let stdin = child.stdin.as_mut().unwrap();
-    let mut stdout = BufReader::new(child.stdout.take().unwrap());
-    let img = image::open(&Path::new("data/lena512.bmp")).unwrap();
-    let (width, height) = img.dimensions();
+    {
+        let stdin = child.stdin.as_mut().unwrap();
+        let mut stdout = BufReader::new(child.stdout.take().unwrap());
+        let img = image::open(&Path::new("data/lena512.bmp")).unwrap();
+        let (width, height) = img.dimensions();
 
-    let child = thread::spawn(move || {
-        let mut buf = String::new();
-        let mut img = ImageBuffer::new(width, height);
-        for y in 0..height {
-            for x in 0..width {
-                stdout.read_line(&mut buf).unwrap();
-                let value = json::parse(&buf).unwrap().pop().as_u8().unwrap();
-                buf.clear();
-                img.put_pixel(x, y, image::Luma([value]));
+        let write_thread = thread::spawn(move || {
+            let mut buf = String::new();
+            let mut img = ImageBuffer::new(width, height);
+            for y in 0..height {
+                for x in 0..width {
+                    stdout.read_line(&mut buf).unwrap();
+                    let value = json::parse(&buf).unwrap().pop().as_u8().unwrap();
+                    buf.clear();
+                    img.put_pixel(x, y, image::Luma([value]));
+                }
+                println!("y = {}", y);
             }
-            println!("y = {}", y);
+            img.save(&Path::new("data/out.png")).unwrap();
+        });
+
+        for (x, y, pixel) in img.pixels() {
+            stdin.write(format!("[{}]\n", pixel.to_luma()[0]).as_bytes()).unwrap();
+            stdin.flush().unwrap();
         }
-        img.save(&Path::new("data/out.png")).unwrap();
-    });
-
-    for (x, y, pixel) in img.pixels() {
-        stdin.write(format!("[{}]\n", pixel.to_luma()[0]).as_bytes()).unwrap();
+        stdin.write(b"\n").unwrap();
         stdin.flush().unwrap();
-    }
-    stdin.write(b"\n").unwrap();
-    stdin.flush().unwrap();
 
-    let res = child.join();
-    res.unwrap();
+        let res = write_thread.join();
+        res.unwrap();
+    }
+
+    child.kill().expect("could not end simulation");
 }
